586415291bbbd797a791029a60aa097d0a3abd60
Sakaria Pouke <sakaria.pouke@qt.io>
2024-11-18 14:49:43 +0200

    formatting

--- config/keymap_finnish.h
+++ config/keymap_finnish.h
@@ -1,45 +1,43 @@
 // Aliases
-#define FI_SECT GRAVE  // §
-#define FI_PLUS MINUS // +
-#define FI_ACUT EQUAL  // ´ (dead)
-#define FI_ARNG LBRC // Å
-#define FI_DIAE RBRC // ¨ (dead)
-#define FI_ODIA SEMI // Ö
-#define FI_ADIA SQT // Ä
-#define FI_QUOT NUHS // '
-#define FI_LABK NUBS // <
-#define FI_COMM COMMA // ,
-#define FI_DOT  DOT  // .
-#define FI_MINS SLASH // -
-#define FI_EXLM LS(N1)    // !
-#define FI_DQUO LS(N2)    // "
-#define FI_HASH LS(N3)    // #
-#define FI_CURR LS(N4)    // ¤
-#define FI_PERC LS(N5)    // %
-#define FI_AMPR LS(N6)    // &
-#define FI_SLSH LS(N7)    // /
-#define FI_LPRN LS(N8)    // (
-#define FI_RPRN LS(N9)    // )
-#define FI_EQL  LS(N0)    // =
+#define FI_SECT GRAVE       // §
+#define FI_PLUS MINUS       // +
+#define FI_ACUT EQUAL       // ´ (dead)
+#define FI_ARNG LBRC        // Å
+#define FI_DIAE RBRC        // ¨ (dead)
+#define FI_ODIA SEMI        // Ö
+#define FI_ADIA SQT         // Ä
+#define FI_QUOT NUHS        // '
+#define FI_LABK NUBS        // <
+#define FI_COMM COMMA       // ,
+#define FI_DOT DOT          // .
+#define FI_MINS SLASH       // -
+#define FI_EXLM LS(N1)      // !
+#define FI_DQUO LS(N2)      // "
+#define FI_HASH LS(N3)      // #
+#define FI_CURR LS(N4)      // ¤
+#define FI_PERC LS(N5)      // %
+#define FI_AMPR LS(N6)      // &
+#define FI_SLSH LS(N7)      // /
+#define FI_LPRN LS(N8)      // (
+#define FI_RPRN LS(N9)      // )
+#define FI_EQL LS(N0)       // =
 #define FI_QUES LS(FI_PLUS) // ?
-#define FI_GRV  LS(FI_ACUT) // ` (dead)
+#define FI_GRV LS(FI_ACUT)  // ` (dead)
 #define FI_CIRC LS(FI_DIAE) // ^ (dead)
 #define FI_ASTR LS(FI_QUOT) // *
 #define FI_RABK LS(FI_LABK) // >
 #define FI_SCLN LS(FI_COMM) // ;
 #define FI_COLN LS(FI_DOT)  // :
 #define FI_UNDS LS(FI_MINS) // _
-#define FI_AT   RA(N2)    // @
-#define FI_PND  RA(N3)    // £
-#define FI_DLR  RA(N4)    // $
-#define FI_EURO RA(N5)    // €
-#define FI_LCBR RA(N7)    // {
-#define FI_LBRC RA(N8)    // [
-#define FI_RBRC RA(N9)    // ]
-#define FI_RCBR RA(N0)    // }
+#define FI_AT RA(N2)        // @
+#define FI_PND RA(N3)       // £
+#define FI_DLR RA(N4)       // $
+#define FI_EURO RA(N5)      // €
+#define FI_LCBR RA(N7)      // {
+#define FI_LBRC RA(N8)      // [
+#define FI_RBRC RA(N9)      // ]
+#define FI_RCBR RA(N0)      // }
 #define FI_BSLS RA(FI_PLUS) // (backslash)
 #define FI_TILD RA(FI_DIAE) // ~ (dead)
 #define FI_PIPE RA(FI_LABK) // |
-#define FI_MICR RA(M)    // µ
-
-
+#define FI_MICR RA(M)       // µ
